4da0775068611eb480be0fc47c435d0db40ef234
Zuhair Ahmad <zuhair2945@gmail.com>
2025-07-16 15:23:42 +0530

    mobile bugs

--- components/SplineScene.jsx
+++ components/SplineScene.jsx
@@ -2,9 +2,9 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
-    <div className='spline-scene'> 
+    <div className='spline-scene'>
       <Spline
-        scene="https://prod.spline.design/eib-GOtdUdSjdJJk/scene.splinecode" 
+        scene="https://prod.spline.design/9zAy0jaSE8WRZfiP/scene.splinecode" 
       />
     </div>
   );
